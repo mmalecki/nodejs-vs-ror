61fcbf6df7c677632786cf355d083961ca767da5
Maciej Małecki <maciej.malecki@notimplemented.org>
2011-09-30 10:25:02 +0200

    Initial version

---
+++ server.js
@@ -0,0 +1,69 @@
+var http = require('http'),
+    async = require('async'),
+    request = require('request');
+
+// ghetto called, they want their template back!
+var template = 
+  "<!DOCTYPE html>" +
+  "<html>" +
+  "  <head>" + 
+  "    <meta charset='utf-8' />" +
+  "    <title>node.js vs RoR</title>" +
+  "    <link rel='stylesheet' href='http://twitter.github.com/bootstrap/1.3.0/bootstrap.min.css'>" +
+  "    <style type='text/css'>" +
+  "      .span8 {" +
+  "        text-align: center;" +
+  "      }" +
+  "    </style>" +
+  "  </head>" +
+  "  <body>" +
+  "    <div class='container'>" +
+  "      <div class='row'>" +
+  "        <div class='span8'>" +
+  "          <h1><a href='https://github.com/joyent/node'>node.js</a></h1>" +
+  "          <h2>%(nodejs)</h2>" +
+  "        </div>" +
+  "        <div class='span8'>" +
+  "          <h1><a href='https://github.com/rails/rails'>Ruby on Rails</a></h1>" +
+  "          <h2>%(ror)</h2>" +
+  "        </div>" +
+  "      </div>" +
+  "      <div class='row'>" +
+  "        <div class='span4 offset12'>" +
+  "          <p>by Maciej Małecki: <a href='https://twitter.com/maciejmalecki'>Twitter</a>, <a href='https://github.com/mmalecki'>GitHub</a>" +
+  "        </div>" +
+  "      </div>" +
+  "    </div>" +
+  "  </body>" +
+  "</html>";
+
+http.createServer(function (req, res) {
+  var ror, nodejs;
+
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  async.parallel({
+    ror: function (callback) {
+      request(
+        'http://github.com/api/v2/json/repos/show/rails/rails',
+        function (error, response, body) {
+          ror = JSON.parse(body).repository.watchers;
+          callback(null, ror);
+        }
+      );
+    },
+    nodejs: function (callback) {
+      request(
+        'http://github.com/api/v2/json/repos/show/joyent/node',
+        function (error, response, body) {
+          nodejs = JSON.parse(body).repository.watchers;
+          callback(null, nodejs);
+        }
+      );
+    }
+  }, function (err, watchers) {
+    res.end(template.replace('%(nodejs)', watchers.nodejs).replace('%(ror)', watchers.ror)); // lol
+  });
+}).listen(8000);
+
+console.log('http server running on port 8000');
+
