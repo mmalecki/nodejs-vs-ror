e238a91de7aafef585bc4028e34fd1af4a6f2820
Maciej Małecki <maciej.malecki@notimplemented.org>
2011-09-30 18:05:54 +0200

    [api] API with JSONP

--- server.js
+++ server.js
@@ -1,121 +1,37 @@
 var http = require('http'),
+    url = require('url'),
     async = require('async'),
-    request = require('request'),
-    cacheInterval = 1000 * 60;
-
-var cache = {};
-setInterval(function () { cache = {}; }, cacheInterval);
-
-// ghetto called, they want their template back!
-var template = 
-  "<!DOCTYPE html>" +
-  "<html>" +
-  "  <head>" + 
-  "    <meta charset='utf-8' />" +
-  "    <title>node.js vs RoR</title>" +
-  "    <link rel='stylesheet' href='http://twitter.github.com/bootstrap/1.3.0/bootstrap.min.css'>" +
-  "    <style type='text/css'>" +
-  "      html, body {" +
-  "        height: 100%;" +  // indeed, CSS.
-  "      }" +
-  "      .container {" +
-  "        position: relative;" +
-  "        top: 40%;" +
-  "      }" +
-  "      h1 {" +
-  "        font-size: 52px" +
-  "      }" +
-  "      h2 {" +
-  "        font-size: 42px;" +
-  "      }" +
-  "      .span8, .span16 {" +
-  "        text-align: center;" +
-  "      }" +
-  "      .bottom {" +
-  "        position: absolute;" +
-  "        bottom: 0px;" +
-  "        right: 0px;" +
-  "        background-color: #98AFC7;" +
-  "        padding: 8px;" +
-  "        border-top-left-radius: 8px;" +
-  "      }" +
-  "    </style>" +
-  "    <script type='text/javascript'>" +
-  "      var _gaq = _gaq || [];" +
-  "      _gaq.push(['_setAccount', 'UA-26044380-1']);" +
-  "      _gaq.push(['_trackPageview']);" +
-  "      (function() {" +
-  "        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;" +
-  "        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';" +
-  "        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);" +
-  "      })();" +
-  "    </script>" +
-  "  </head>" +
-  "  <body>" +
-  "    <div class='container'>" +
-  "      <div class='row'>" +
-  "        <div class='span8'>" +
-  "          <h1><a href='https://github.com/joyent/node'>node.js</a></h1>" +
-  "          <h2>%(nodejs)</h2>" +
-  "          <p>GitHub watchers<p>" +
-  "        </div>" +
-  "        <div class='span8'>" +
-  "          <h1><a href='https://github.com/rails/rails'>Ruby on Rails</a></h1>" +
-  "          <h2>%(ror)</h2>" +
-  "          <p>GitHub watchers<p>" +
-  "        </div>" +
-  "      </div>" +
-  "      <div class='row'>" +
-  "        <div class='span16'>" +
-  "          <h1 style='margin-top: 128px'>%(text)</h1>" +
-  "        </div>" +
-  "      </div>" +
-  "    </div>" +
-  "    <div class='bottom'>" +
-  "      <p style='margin: 0px'>" +
-  "        by Maciej Małecki: <a rel='me' href='https://twitter.com/maciejmalecki'>Twitter</a>, <a rel='me' href='https://github.com/mmalecki'>GitHub</a>" +
-  "        <br />" +
-  "        <a href='https://github.com/mmalecki/nodejs-vs-ror'>Source</a>" +
-  "      </p>" +
-  "    </div>" +
-  "  </body>" +
-  "</html>";
+    request = require('request');
 
 http.createServer(function (req, res) {
-  var ror, nodejs;
-
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  if (cache.output) {
-    return res.end(cache.output);
+  var parsed = url.parse(req.url, true);
+  if (parsed.pathname == '/api') {
+    async.parallel({
+      ror: function (callback) {
+        request(
+          'http://github.com/api/v2/json/repos/show/rails/rails',
+          function (error, response, body) {
+            callback(null, JSON.parse(body).repository.watchers);
+          }
+        );
+      },
+      nodejs: function (callback) {
+        request(
+          'http://github.com/api/v2/json/repos/show/joyent/node',
+          function (error, response, body) {
+            callback(null, JSON.parse(body).repository.watchers);
+          }
+        );
+      }
+    }, function (err, watchers) {
+      var response = JSON.stringify(watchers);
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      if (parsed.query.jsonp) {
+        return res.end(parsed.query.jsonp + '(' + response + ');');
+      }
+      res.end(response);
+    });
   }
-
-  async.parallel({
-    ror: function (callback) {
-      request(
-        'http://github.com/api/v2/json/repos/show/rails/rails',
-        function (error, response, body) {
-          ror = JSON.parse(body).repository.watchers;
-          callback(null, ror);
-        }
-      );
-    },
-    nodejs: function (callback) {
-      request(
-        'http://github.com/api/v2/json/repos/show/joyent/node',
-        function (error, response, body) {
-          nodejs = JSON.parse(body).repository.watchers;
-          callback(null, nodejs);
-        }
-      );
-    }
-  }, function (err, watchers) {
-    var text = (watchers.ror > watchers.nodejs) ? "Not quite there... yet!" : "Wooooooooo!";
-    cache.output = template
-                   .replace('%(nodejs)', watchers.nodejs)
-                   .replace('%(ror)', watchers.ror)
-                   .replace('%(text)', text);
-    res.end(cache.output);
-  });
 }).listen(8000);
 
 console.log('http server running on port 8000');
